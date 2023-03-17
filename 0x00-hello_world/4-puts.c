62a59fe2d7bb326a66d6ddfc666959d281fb07ac
root <alouismariea97@gmail.com>
2023-03-16 17:31:57 -0700

    Committing Changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry pount
+ * Return: Always 0 (success)
+ * This code prints a string to stdout using puts()
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
