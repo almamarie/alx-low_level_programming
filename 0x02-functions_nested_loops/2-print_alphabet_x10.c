01f10764ffbf08cbd08033c5966bf8ef94764b00
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-21 21:08:31 +0000

    wrote a function that print the alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ * Return: on success, 0, on error, 1
+ * Description: prints the alphabets in lowercase 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+char i;
+int j;
+for (j = 0; j < 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
