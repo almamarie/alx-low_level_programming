b0c68b45bf0e60bc52f04a7258665ab332b63f86
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-21 21:20:17 +0000

    Added a function that checks whether a char is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - entry point
+ * @c: a character
+ * Return: 1 if c is lowercase and 0 otherwise
+*/
+
+int _islower(char c)
+{
+return (islower(c));
+}
