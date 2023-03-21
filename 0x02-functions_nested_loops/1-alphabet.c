839c0fc5c6cf5ae30d5834951d3efd879c5c26d3
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-21 20:51:32 +0000

    wrote a function to print the alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: success 0, failure 1
+ * description: prints the alphabets in lower case
+*/
+
+int main(void)
+{
+print_alphabet();
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: on success, 0, on error, 1
+ * description: prints the alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+}
