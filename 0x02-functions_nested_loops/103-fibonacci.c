403ac56fce2414fce9b8d236d68eb5aa0b87470c
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-22 01:37:46 +0000

    Finished with task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: void
+ * description: prints sum of even fibonacci numbers
+*/
+
+int main(void)
+{
+
+unsigned long acc = 0;
+int i;
+long fib[35];
+fib[0] = 1;
+fib[1] = 2;
+for (i = 2; i < 35; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+
+for (i = 0; i < 35; i++)
+{
+if (fib[i] < 4000000)
+{
+acc += fib[i];
+}
+}
+
+
+
+
+
+printf("%lu \n", acc);
+
+return (0);
+}
