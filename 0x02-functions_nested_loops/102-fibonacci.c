0858c8c0cfe67cc0fc04dad015bfb85361da82ae
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-22 01:18:29 +0000

    fixed betty error

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,9 +11,9 @@ int i;
 long fib[50];
 fib[0] = 1;
 fib[1] = 2;
-for (i =2; i < 50; i++)
+for (i = 2; i < 50; i++)
 {
-fib[i] = fib[i-1] + fib[i-2];
+fib[i] = fib[i - 1] + fib[i - 2];
 }
 
 for (i = 0; i < 50; i++)
