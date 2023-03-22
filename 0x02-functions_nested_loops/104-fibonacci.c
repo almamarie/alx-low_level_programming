e57e477db67207802195661e1fab5f21b9e6d22f
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-22 02:26:20 +0000

    fixed task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,24 @@
 int main(void)
 {
 int i;
-unsigned long int fib;
-unsigned long int prev = 1;
-unsigned long int curr = 2;
+long double fib;
+long double prev = 1;
+long double curr = 2;
 
 printf("1, 2, ");
 
-for (i = 2; i < 99; i++)
+for (i = 3; i < 99; i++)
 {
 fib = prev + curr;
 prev = curr;
 curr = fib;
 
-printf("%lu", fib);
+printf("%.0Lf", fib);
 if (i != 98)
 {
 printf(", ");
 }
 }
-
-
 
 printf("\n");
 return (0);
