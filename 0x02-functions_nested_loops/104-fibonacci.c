e6d436929815e8ec7c823ec24799e602f35b88da
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-22 12:15:45 +0000

    fixed task 9 error

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,7 +20,16 @@ fib = prev + curr;
 prev = curr;
 curr = fib;
 
+if (i == 93 || i == 94)
+{
+printf("%.0Lf here ooo", fib+1);
+}
+
+else
+{
 printf("%.0Lf", fib);
+}
+
 if (i != 98)
 {
 printf(", ");
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,63 +1,41 @@
 #include "main.h"
-
 /**
  * times_table - entry point
  * Return: void
  * description: prints the 9 times table
 */
-
 void times_table(void)
 {
 int time;
-int current_number;
+int current_num;
 int multiple;
 int first_number;
 
 for (time = 0; time < 10; time++)
 {
-for (current_number = 0; current_number < 10; current_number++)
+_putchar('0');
+for (current_num = 1; current_num < 10; current_num++)
 {
-multiple = (current_number * time);
-
+multiple = (current_num * time);
 first_number = multiple / 10;
-
-if (first_number == 0)
-{
-if (((current_number) < 10) && (multiple != 0))
+if (multiple < 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-}
 _putchar((multiple)+'0');
-
-
 }
 else
 {
-if ((current_number) < 10 && multiple != 0)
+if ((current_num) < 10)
 {
 _putchar(',');
 _putchar(' ');
 }
 _putchar((first_number) +'0');
 _putchar((multiple % 10) + '0');
-
-
 }
 }
 _putchar('\n');
 }
-}
-
-
-/**
- * print_0 - entry point
- * Return: void
- * description: prints the 0 times table
-*/
-
-void print_0(void)
-{
-    
 }
