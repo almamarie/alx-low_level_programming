a992e9a7a574a58741e88e60cf81caa87934b3e5
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-23 19:48:39 +0000

    Completed task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - entry point
+ * Return: 1 if is upper, 0 otherwise
+ * @c: integer input
+ * Description: checks if the input is upper
+*/
+
+int _isupper(int c)
+{
+return (isupper(c));
+}
