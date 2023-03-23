8c2549da732a6ce0fbfcceccea560be3931ecb39
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-23 20:24:49 +0000

    Completed task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Return: void
+ * Description: prints the numbers from 0 to 9
+ * 10 times
+*/
+
+void more_numbers(void)
+{
+int i;
+int j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i > 9)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+continue;
+}
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+}
