054011c61a34f3933ee2ddf8d3d721a631982355
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-23 23:19:22 +0000

    fixed task 0 error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -9,10 +8,9 @@
 int main(void)
 {
 int i;
-int result;
 
 i = 0;
-result = positive_or_negative(i);
+positive_or_negative(i);
 
 return (0);
 }
