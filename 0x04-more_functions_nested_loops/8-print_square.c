df184bd2fc1b438f53655ee755e57fc9110d24e4
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-23 20:50:23 +0000

    completed task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - entry point
+ * Return: void
+ * @size: integer input
+ * Description: prints a (size x size) matrix
+*/
+
+void print_square(int size)
+{
+int i;
+int j;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < size; i++)
+{
+
+for (j = 0; j < size; j++)
+{
+_putchar('*');
+}
+
+_putchar('\n');
+}
+
+}
