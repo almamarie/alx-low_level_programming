3afd546df0c77ababf9028660648944de2f425b2
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-23 21:24:03 +0000

    fixed _putchar error

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,21 +14,27 @@ int i;
 int spaces;
 int hashes;
 
+if (size < 1)
+{
+_putchar('\n');
+return;
+}
+
 for (i = 1; i < (size + 1); i++)
 {
 
 
 for (spaces = 0; spaces < (size - i); spaces++)
 {
-_putchar(" ");
+_putchar(' ');
 }
 
 
 for (hashes = 0; hashes < i; hashes++)
 {
-_putchar("#");
+_putchar('#');
 }
 
-_putchar("\n");
+_putchar('\n');
 }
 }
