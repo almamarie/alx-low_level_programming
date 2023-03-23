4fce7b29609318fff21c013389cb66f1e8d2565a
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-23 20:05:40 +0000

    Completed task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * Return: 1 if is upper, 0 otherwise
+ * @a: integer input
+ * @b: integer input
+ * Description: multiplies the inputs
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
