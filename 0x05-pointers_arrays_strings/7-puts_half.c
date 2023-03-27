3ed801b51a53749d63dcd5e7714879e97174127d
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-27 22:56:45 +0000

    completed task 8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
  * puts_half - entry point
  * Return: void
@@ -15,6 +16,10 @@ int str_len;
 
 str_len = strlen(str);
 
+
+printf("\nstring length: %d\n", str_len  / 2);
+
+
 if (str_len % 2 == 0)
 {
 for (i = str_len / 2; i < str_len; i++)
@@ -24,7 +29,7 @@ _putchar(str[i]);
 } 
 else 
 {
-for (i = ((str_len - 1) / 2); i < str_len; i++)
+for (i = (str_len - 1) / 2; i < str_len; i++)
 {
 _putchar(str[i]);
 }
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_array - entry point
+ * Return: void
+ * description: prints n elements of an array of 
+ * integers, followed by a new line.
+ * @a: *char[] input
+ * @n: the number of elements to print
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i + 1 < n)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d\n", a[i]);
+}
+}
+}
