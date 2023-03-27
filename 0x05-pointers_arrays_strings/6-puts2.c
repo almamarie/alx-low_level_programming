58cfa95405027c7c7f94f6324feecc08df22d203
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-27 22:30:56 +0000

    completed task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - entry point
+ * Return: void
+ * description: prints every other character of a string,
+ * starting with the first character, followed by a new line
+*/
+
+void puts2(char *str)
+{
+
+int i;
+int str_len;
+
+str_len = strlen(str);
+
+for (i = 0; i < str_len; i = i + 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
