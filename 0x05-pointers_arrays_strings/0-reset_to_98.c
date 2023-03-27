3d03337a2016da7dfd1284a93f276d3233564ce2
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-27 21:02:48 +0000

    Completed task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ * Return: void
+ * description: receives a pointer and
+ * resets the valuse to 98.
+*/
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# C - Pointers, arrays and strings
+
+This foilder contains C pointers, arrays, and strings
