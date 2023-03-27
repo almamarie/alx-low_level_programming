4517f9b6b50994d19d5693f2de7db62e651bb72e
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-27 23:11:39 +0000

    completed task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_array - entry point
+ * Return: a pointer to an array
+ * description: concatenates a string
+ * @a: *char[] input
+ * @n: the number of elements to print
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+
+return dest;
+}
