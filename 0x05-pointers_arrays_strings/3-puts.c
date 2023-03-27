59adb688e46b537288438b00c8473f5404872f1a
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-27 21:29:59 +0000

    completed task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _puts - entry point
+ * Return: void
+ * description: prints a string to stdout
+ * @str: an array of char
+*/
+
+void _puts(char *str)
+{
+puts(str);
+}
