738501dc56998dbf7e3e6f92f74e46208c110fa9
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-03-30 00:32:40 +0000

    fixed bug

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,24 +1,22 @@
-#include <string.h>
 /**
- * _strcmp - entry bpoint
+ * reverse_array - entry bpoint
  * Return: integer
- * description: concatinates 2 strings up to n bytes
- * @s1: string 1
- * @s2: string 2
+ * description: reverses a string
+ * @a: array to reverse
+ * @n: size of @a
 */
 
 void reverse_array(int *a, int n)
 {
 
-// copy the string
 int i;
 int tmp;
 
 for (i = 0; i < (n / 2); i++)
 {
 tmp = a[i];
-a[i] = a[n - i -1];
-a[n - i -1] = tmp;
+a[i] = a[n - i - 1];
+a[n - i - 1] = tmp;
 }
 
 }
