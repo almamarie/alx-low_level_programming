6eabdf367546593d0476ecdf1ab5cf0f29a64bc6
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-03 22:10:48 +0000

    completed task 11

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: pointer to the string
+ * @to: string to set
+ * Return: void
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
