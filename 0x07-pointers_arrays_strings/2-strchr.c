e4054a4c22fc7d703a7166394f25b4c58aaae5f2
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-03 18:05:46 +0000

    completed task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: character to locate
+ * Return: a pointer to the first occurrence of @c
+*/
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+
+return (NULL);
+}
