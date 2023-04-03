2846137287f6638b7c986785437102a6d502e44f
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-03 19:59:01 +0000

    Completed task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+/**
+ * _strstr -  searches a string for any of a set of bytes
+ * @haystack: src string
+ * @needle: substring to locate
+ * Return: a pointer to the beginning of the located
+ * substring or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+while (*haystack)
+{
+if (*haystack == *needle)
+{
+return (needle);
+}
+haystack++;
+}
+
+return (NULL);
+}
