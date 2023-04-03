0b3eecaaf2245dbfb8f19429f81ac0560d06c5d4
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-03 17:36:51 +0000

    completed task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+
+/**
+ * _memset - entry point
+ * Return: a pointer to the memory area s
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes to fill
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+    s[i] = b;
+}
+
+return s;
+}
