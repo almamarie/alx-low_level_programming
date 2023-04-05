07a5696c95a2ac4ddfc3689b6197416f5a3a0972
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-05 09:26:23 +0000

    Completed task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,29 @@
+
 /**
- * _sqrt_recursion - finds the natural 
+ * _sqrt - finds the natural sqrt of @n
+ * square root of a number
+ * @n: the number
+ * @v: the iterator
+ * Return: integer
+*/
+
+int _sqrt(int n, int v)
+{
+if (v * v > n)
+{
+return (-1);
+}
+
+if (v * v == n)
+{
+return (v);
+}
+
+return (_sqrt(n, v + 1));
+}
+
+/**
+ * _sqrt_recursion - finds the natural
  * square root of a number
  * @n: the number
  * Return: integer
@@ -7,5 +31,10 @@
 
 int _sqrt_recursion(int n)
 {
-
+if (n <= 0)
+{
+return (-1);
 }
+return (_sqrt(n, 0));
+}
+
