0e234143affc922cea48a295141ffdbf7e707c7e
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-10 21:52:49 +0000

    Fixed betty error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,12 @@
 /**
  * main - entry point
  * Return: Always (0)
- * 
+ * @argc: number of arguements
+ * @argv: vector of arguements
+ *
 */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 if (argc < 0)
 {
