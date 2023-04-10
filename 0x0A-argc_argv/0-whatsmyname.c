4f906b013cdf06f34e086443582abeeea0318f81
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-10 21:58:52 +0000

    fixed error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point
@@ -8,12 +9,8 @@
  *
 */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-if (argc < 0)
-{
-return (1);
-}
 printf("%s\n", argv[0]);
 return (0);
 }
