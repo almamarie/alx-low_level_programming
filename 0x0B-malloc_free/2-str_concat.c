4fc8a8a9089d837ca9875dd902f828273f79e2d9
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-11 22:43:15 +0000

    Changed algorithm

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -52,11 +52,21 @@ char *str_concat(char *s1, char *s2)
 {
 int size1;
 int size2;
-int size3;
+int i, j;
 char *result;
 
-size1 = s1 == NULL ? 0 : _size(s1);
-size2 = s2 == NULL ? 0 : _size(s2);
+if (s1 == NULL)
+{
+s1 = "";
+}
+
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+size1 = _size(s1);
+size2 = _size(s2);
 
 if (s1 == 0 && s2 == 0)
 {
@@ -65,26 +75,50 @@ result[0] = '\0';
 return (result);
 }
 
-if (size1 > 0 && size2 > 0)
+result = malloc((size1 + size2) * (sizeof(char)));
+
+if (result == NULL)
 {
-size3 = size1 + size2;
-result = malloc(size3 * (sizeof(char)));
-_concat(result, 0, s1, size1);
-_concat(result, size1, s2, (size1 + size2));
+return (NULL);
 }
 
-if (size1 > 0 && size2 <= 0)
+i = 0;
+while (s1[i] != '\0')
 {
-result = malloc(size1 * (sizeof(char)));
-_concat(result, 0, s1, size1);
+result[i] = s1[i];
+i++;
 }
 
-if (size1 <= 0 && size2 > 0)
+j = 0;
+while (s2[j] != '\0')
 {
-result = malloc(size2 * (sizeof(char)));
-_concat(result, 0, s2, size2);
+result[i] = s2[j];
+i++;
+j++;
 }
 
 result[size1 + size2] = '\n';
-return (result);
+return result;
+// if (size1 > 0 && size2 > 0)
+// {
+// size3 = size1 + size2;
+// result = malloc(size3 * (sizeof(char)));
+// _concat(result, 0, s1, size1);
+// _concat(result, size1, s2, (size1 + size2));
+// }
+
+// if (size1 > 0 && size2 <= 0)
+// {
+// result = malloc(size1 * (sizeof(char)));
+// _concat(result, 0, s1, size1);
+// }
+
+// if (size1 <= 0 && size2 > 0)
+// {
+// result = malloc(size2 * (sizeof(char)));
+// _concat(result, 0, s2, size2);
+// }
+
+// result[size1 + size2 + 1] = '\0';
+// return (result);
 }
