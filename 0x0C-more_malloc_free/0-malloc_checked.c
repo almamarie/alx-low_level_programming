9c6941ec8359504c453caa035b8d5ee67f4934b4
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-13 23:53:35 +0000

    Completed task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: the value
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *p;
+
+p = malloc(b);
+
+if (p == NULL)
+{
+exit(98);
+}
+
+return p;
+}
