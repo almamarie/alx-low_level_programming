d4e436a52ad2626a9ce2e9daff72b2cd3c54da1c
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-14 00:38:27 +0000

    Changed algorithm

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,6 +9,7 @@
 
 int *array_range(int min, int max)
 {
+int i, j;
 int *array;
 int range;
 
@@ -18,16 +19,23 @@ return (NULL);
 }
 
 range = max - min + 1;
-array = malloc((range) * sizeof(int));
+array = malloc(range * sizeof(int));
 
 if (array == NULL)
 {
 return (NULL);
 }
 
+/*
 for (; range > 0; range--)
 {
 array[range - 1] = range - 1;
+}
+*/
+
+for (i = 0, j = min; i < range; i++, j++)
+{
+array[i] = j;
 }
 
 return (array);
