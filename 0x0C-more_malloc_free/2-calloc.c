998b50ce71b766cd8bca1819afd343d441a06fa8
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-14 00:13:50 +0000

    completed task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: umber of elements
+ * @size: size of the array
+ * Return: pointer to the allocated space or NULL
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *p;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+
+p = malloc(nmemb * size);
+
+if (p == NULL)
+{
+return (NULL);
+}
+
+return (p);
+}
