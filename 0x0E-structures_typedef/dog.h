c4411cf4c84e14219a6b1e0da4802df9d0b23308
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-17 20:16:29 +0000

    Fixed betty warnings and header file warnings

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the name of the owner of the dog
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+#endif
