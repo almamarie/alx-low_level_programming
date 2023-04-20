d123891625a36522cc07a23903bbd8a040850f74
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-20 00:36:19 +0000

    Completed task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+
+
+/**
+ * print_name - prints a character
+ * @name: string to print
+ * @f: function to use to print string
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
