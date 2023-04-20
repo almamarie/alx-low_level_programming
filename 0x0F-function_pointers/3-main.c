a012f8ac781a67f81892bc85ea350c20e496b1c3
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-20 01:17:51 +0000

    Completed task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * main - performs an operation on 2 numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 for Success.
+ */
+int main(int argc, char *argv[])
+{
+
+int a, b, result;
+char *op = argv[2];
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+a = atoi(argv[1]), b = atoi(argv[3]);
+
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+
+if ((*op == '/' || *op == '%') && b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+result = get_op_func(op)(a, b);
+printf("%d\n", result);
+
+return (0);
+}
