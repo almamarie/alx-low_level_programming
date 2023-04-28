e9b4e8ba07fe9f6ac1cfc108bde5e3c2eb778b26
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-28 20:00:04 +0000

    Testing assignment 10

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+section .data
+    msg: db 'Hello, Holberton!', 10, 0
+
+section .text
+    extern printf
+
+global main
+
+main:
+    ; Clear the RAX register to prepare for calling printf
+    xor rax, rax
+
+    ; Pass the message address as argument to printf
+    lea rdi, [msg]
+
+    ; Call printf to print the message
+    call printf
+
+    ; Set RAX to 0 (the exit status) to prepare for returning from main
+    xor rax, rax
+
+    ; Return from main
+    ret
