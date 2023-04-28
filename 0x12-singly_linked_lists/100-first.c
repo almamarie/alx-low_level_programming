9818aa07575d2d756253a6bb3799e30272bc5887
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-28 10:53:52 +0000

    Fixed betty error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+/**
+ * print_before_main - runs before main
+*/
+
 void __attribute__((constructor)) print_before_main(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,");
+printf("I bore my house upon my back!\n");
+
 }
