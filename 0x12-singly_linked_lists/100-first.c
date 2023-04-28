d15c79a653660e485f721327d92f9f7e7b7acd5e
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-28 10:54:53 +0000

    Fixed error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 void __attribute__((constructor)) print_before_main(void)
 {
 printf("You're beat! and yet, you must allow,");
-printf("I bore my house upon my back!\n");
+printf("\nI bore my house upon my back!\n");
 
 }
