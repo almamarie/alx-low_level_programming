b6d20cb06310a76e9ef5ea4133c6e2035bb7b881
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-04-28 20:16:02 +0000

    Completed task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints the elements in a list
+ * @h: the linkedlist
+ * Return: the size of the list
+*/
+
+size_t print_listint(const listint_t *h)
+{
+size_t size;
+
+size = 0;
+
+while (h)
+{
+printf("%u\n", h->n);
+size++;
+h = h->next;
+}
+
+return (size);
+}
